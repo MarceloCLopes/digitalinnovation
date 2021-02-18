334104259fad3206ad1c31276523a792ceaa10f0
MarceloCLopes <marc01mcl@gmail.com>
2021-02-18 10:57:37 -0300

    updated to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,13 +4,15 @@ class Pessoa {
     var name:String = "Marcelo"
     var cpf: String = "123.123.123-12"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$name e $cpf"
 }
 
 fun main(){
     val marcelo = Pessoa()
     //marcelo.cpf = "5469"
 
-    println(marcelo)
-    println(marcelo.name)
-    println(marcelo.cpf)
+    println(marcelo.pessoaInfo())
 }
