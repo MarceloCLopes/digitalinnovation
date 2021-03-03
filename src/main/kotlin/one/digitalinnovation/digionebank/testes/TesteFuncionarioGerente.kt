13ae10bdd1b6216158bfd4348456e706b06c3e03
MarceloCLopes <marc01mcl@gmail.com>
2021-03-03 14:22:39 -0300

    update test employee and manager

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -3,8 +3,10 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
-    val maria = Gerente(name = "Maria Claro Lopes", cpf = "98765432100", 5000.00)
+    val maria = Gerente(name = "Maria Claro Lopes", cpf = "98765432100", 5000.00, "senha123")
 
     ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
 
