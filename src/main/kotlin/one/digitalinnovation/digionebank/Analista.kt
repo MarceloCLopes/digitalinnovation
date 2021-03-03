b5125af2f114e16d8b5ebd55ba23127e7223eb6f
MarceloCLopes <marc01mcl@gmail.com>
2021-03-03 12:47:49 -0300

    created sub classes manager and analyst

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Analista.kt
@@ -0,0 +1,9 @@
+package one.digitalinnovation.digionebank
+
+class Analista(
+    name: String,
+    cpf: String,
+    salario: Double
+) : Funcionario(name, cpf, salario) {
+    override fun calculoAuxilio() = salario * 0.1
+}
